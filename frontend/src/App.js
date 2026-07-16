8d22efe8e9af839be3ac86a258f00975d2bf6bc3
Your Name <you@example.com>
2026-07-16 23:54:57 +0530

    Remove unused Breadcrum import

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,13 +12,13 @@ import Footer from './Components/Footer/Footer';
 import men_banner from './Components/Assets/Assets/Frontend_Assets/banner_mens.png'
 import women_banner from './Components/Assets/Assets/Frontend_Assets/banner_women.png'
 import kid_banner from './Components/Assets/Assets/Frontend_Assets/banner_kids.png'
-import Breadcrum from './Components/Breadcrums/Breadcrum.jsx';
+// import Breadcrum from './Components/Breadcrums/Breadcrum.jsx';
 function App() {
   return (
     <div>
       <BrowserRouter>
       <Navbar/>
-      <Breadcrum/>
+      {/* <Breadcrum/> */}
       <Routes>
         <Route path='/' element={<Shop/>}/>
          <Route path='/mens' element={<ShopCategory banner={men_banner}category="men"/>}/>
